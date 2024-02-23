7c28df9d95224a7c466c2328afb5bcd0653c0dc9
Ian Iversen <ianiversen1@gmail.com>
2024-02-23 08:54:57 -0500

    style: css for listcomponenet set

--- react-walkthrough-3/src/App.jsx
+++ react-walkthrough-3/src/App.jsx
@@ -16,6 +16,8 @@ function App() {
       <div className='tile'>item</div>
       <div className='tile'>item</div>
       <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
     </div>
   )
 }
