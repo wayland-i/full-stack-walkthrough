01d4c27cc22907bd4210c323c133d2cc8737c8a7
Ian Iversen <ianiversen1@gmail.com>
2024-02-22 23:36:58 -0500

    css test with flex

--- react-walkthrough-3/src/App.jsx
+++ react-walkthrough-3/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className='outer-box'>
-      <div className='box'></div>
+    <div className='container'>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
+      <div className='tile'>item</div>
     </div>
   )
 }
